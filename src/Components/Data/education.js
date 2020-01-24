7237630e46b3570aba80361cf40005e979b43df4
kenzo <arwysyahputrasiregar@gmail.com>
2020-01-24 22:15:25 +0700

    exchange data education

--- src/Components/Data/education.js
+++ src/Components/Data/education.js
@@ -1,7 +1,8 @@
 const education=[
     {
         location:'Arkademy Tech Education Platform',
-        image_url:"http://res.cloudinary.com/kenzo/image/upload/v1579794252/di1prndnmhknqe06ery1.png",
+        
+        image_url:"http://res.cloudinary.com/kenzo/image/upload/v1579794586/itl2ofgsprvygghced8e.png",
         years:"2019-2020",
         major: "Full Stack Developer"
       },
@@ -12,8 +13,8 @@ const education=[
         major: "Java Mobile Programming"
       },
       {
-        location:'Balai Latihan Kerja Bekasi',
-        image_url:"http://res.cloudinary.com/kenzo/image/upload/v1579794586/itl2ofgsprvygghced8e.png",
+        location:':Politeknik Negeri Medan',
+        image_url:"http://res.cloudinary.com/kenzo/image/upload/v1579794252/di1prndnmhknqe06ery1.png",
         years:"2014-2018",
         major: "Civil Engineering"
       },
